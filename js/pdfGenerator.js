bc36a3bc1f6128c6a2f73e811b393bbe801bd325
Miguel Arrañaga <miguelaf44@gmail.com>
2021-01-18 18:58:14 -0600

    Funcionamiento del boton Descargar

--- js/pdfGenerator.js
+++ js/pdfGenerator.js
@@ -1,35 +1,7 @@
-function getPDF() {
-    // console.log("hola");
-    // const doc = new jsPDF();
-    // var quotes = document.getElementById('container-fluid');
-    // doc.text("Hello world!", 10, 10);
-    // doc.save("a4.pdf");
+$('#print').click(function(){
+  html2canvas(document.querySelector("#capture")).then(canvas => {
+    //document.body.appendChild(canvas)
+    return Canvas2Image.saveAsPNG(canvas);
+  });
 
-    var pdf = new jsPDF('p', 'pt', 'letter');
-    source = $('#imprimir')[0];
-
-    specialElementHandlers = {
-        '#bypassme': function (element, renderer) {
-            return true
-        }
-    };
-    margins = {
-        top: 80,
-        bottom: 60,
-        left: 40,
-        width: 522
-    };
-
-    pdf.fromHTML(
-        source, 
-        margins.left, // x coord
-        margins.top, { // y coord
-            'width': margins.width, 
-            'elementHandlers': specialElementHandlers
-        },
-
-        function (dispose) {
-            pdf.save('Prueba.pdf');
-        }, margins
-    );
-}
+})
